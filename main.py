72b6c034f71ed9e96a5c3d3169f4e167e293f3e8
Vann4 <Vann4>
2023-09-05 00:58:23 +0300

    Выполнил замену четных чисел на строку

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ user_number = int(input('Введите число: '))
 user_string = input('Введите строку: ')
 
 number_list = list(range(1, user_number + 1))
+
+for i in range(len(number_list)):
+    if number_list[i] % 2 == 0:
+        number_list[i] = user_string
