5e6aa10fff8a1e88045887159db6b37860582e51
Vann4 <Vann4>
2023-09-11 22:20:54 +0300

    Создал input и пустой словарь

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-user_number = int(input('Введите число больше нуля: '))
-user_string = input('Введите строку: ')
+# user_number = int(input('Введите число больше нуля: '))
+# user_string = input('Введите строку: ')
+#
+# number_list = list(range(1, user_number + 1))
+#
+# for i in range(len(number_list)):
+#     if number_list[i] % 2 == 0:
+#         number_list[i] = user_string
+#
+# print(number_list)
+# print("Стучусь в контрибьютеры за красивые глаза")
 
-number_list = list(range(1, user_number + 1))
-
-for i in range(len(number_list)):
-    if number_list[i] % 2 == 0:
-        number_list[i] = user_string
-
-print(number_list)
-
-print("Стучусь в контрибьютеры за красивые глаза")
+#Задача: взять текст от пользователя и вывести число повторяющихся букв
+user_string = input('Введите текст на английском языке: ')
+letter_count = {} #Пустой словарь
